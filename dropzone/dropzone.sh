32df2fb5bbdda8b7c69aa332ea59ccef569374c9
mylamour <mylamour@163.com>
2020-12-24 18:17:54 +0800

    replace images mo-vautl:base with python:3.8

--- dropzone/dropzone.sh
+++ dropzone/dropzone.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# nohup python3 /root/app.py && 
-
 # Init New Slot
-
+#pass
 # Init Dropzone HSM Key
-# curl --header "Content-Type: application/json"  --request POST --data '{"secret_path":"dropzone/rsa", "secret_version":"1"}'  http://mo.vault.eaas:8443/gen/key/rsa
-# pkcs11-tool --module /usr/local/lib/softhsm/libsofthsm2.so -l --keypairgen --key-type rsa:4096 --id 1 --label "dropzone/rsa"
 while true; do
 
   curl --header "Content-Type: application/json"  --request POST --data '{"secret_path":"dropzone/rsa", "secret_version":"1"}' http://mo.vault.eaas:8443/gen/key/rsa > log.info
